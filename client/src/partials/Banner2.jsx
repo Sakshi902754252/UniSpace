921b7ecc2afb4f31764c12a231793ea33f515b79
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2024-06-14 15:44:29 +0530

    Mayank : vachl tumhala

--- client/src/partials/Banner2.jsx
+++ client/src/partials/Banner2.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 function Banner2() {
   return (
-    <div className="relative bg-indigo-200 dark:bg-indigo-500 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
+    <div className="relative bg-indigo-200 dark:bg-indigo-800 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
       {/* Background illustration */}
       <div className="absolute right-0 top-0 -mt-4 mr-16 pointer-events-none hidden xl:block" aria-hidden="true">
         <svg width="319" height="198" xmlnsXlink="http://www.w3.org/1999/xlink">
